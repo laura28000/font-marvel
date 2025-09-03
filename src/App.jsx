90aae241601dd5eba4f025d4c44a6e5790a0a60b
Caprara Laura <capraralaura@MacBook-Air-de-Caprara.local>
2025-09-03 20:38:46 +0200

    Fix casing: rename 'Pages' to 'pages' for Netlify compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Characters from "./Pages/Characters";
-import Comics from "./pages/Comics";
-import CharacterDetail from "./pages/CharacterDetail";
-import Favorites from "./pages/Favorites";
+import Comics from "./Pages/Comics";
+import CharacterDetail from "./Pages/CharacterDetail";
+import Favorites from "./Pages/Favorites";
 import "./App.css";
 
 function App() {
